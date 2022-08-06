79b7b4dcac35a8a5a75f783111043dfbebdd6f2a
Niveditamoorthy <110727135+Niveditamoorthy@users.noreply.github.com>
2022-08-06 09:14:38 -0700

    Add files via upload
    Week1

---
+++ day1/student.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class student {
+	public static void main(String[]args) {
+		String studentName="Nivedita";
+		int rollNo=20;
+		String collegeName="SRM";
+		int markScored=85;
+		float ccpa=8.5f;
+		
+		System.out.println(studentName);
+		System.out.println(rollNo);
+		System.out.println(collegeName);
+		System.out.println(markScored);
+		System.out.println(ccpa);
+		
+	}
+}
